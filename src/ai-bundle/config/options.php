symfony/ai

feat(platform|agent): add support for Speech

src/ai-bundle/config/options.php
@@ -310,6 +310,38 @@ return static function (DefinitionConfigurator $configurator): void {
                             ->info('Continue the agent run even if a tool call fails')
                             ->defaultTrue()
                         ->end()
+                        ->arrayNode('speech')
+                            ->info('Speech configuration for the agent (TTS/STT)')
+                            ->addDefaultsIfNotSet()
+                            ->treatFalseLike(['enabled' => false])
+                            ->treatTrueLike(['enabled' => true])
+                            ->treatNullLike(['enabled' => false])
+                            ->children()
+                                ->booleanNode('enabled')->defaultFalse()->end()
+                                ->stringNode('platform')
+                                    ->info('Platform service name to use for speech (e.g. "ai.platform.elevenlabs")')
+                                    ->defaultNull()
+                                ->end()
+                                ->stringNode('tts_model')
+                                    ->info('Model name for text-to-speech')
+                                    ->defaultNull()
+                                ->end()
+                                ->arrayNode('tts_options')
+                                    ->info('Options for text-to-speech')
+                                    ->variablePrototype()->end()
+                                    ->defaultValue([])
+                                ->end()
+                                ->stringNode('stt_model')
+                                    ->info('Model name for speech-to-text')
+                                    ->defaultNull()
+                                ->end()
+                                ->arrayNode('stt_options')
+                                    ->info('Extra options for speech-to-text')
+                                    ->variablePrototype()->end()
+                                    ->defaultValue([])
+                                ->end()
+                            ->end()
+                        ->end()
                     ->end()
                 ->end()
             ->end()

Guikingone/symfony-ai

src/ai-bundle/config/options.php
@@ -310,6 +310,43 @@ return static function (DefinitionConfigurator $configurator): void {
                             ->info('Continue the agent run even if a tool call fails')
                             ->defaultTrue()
                         ->end()
+                        ->arrayNode('speech')
+                            ->info('Speech (TTS/STT) decorator configuration')
+                            ->treatFalseLike(['enabled' => false])
+                            ->treatTrueLike(['enabled' => true])
+                            ->treatNullLike(['enabled' => false])
+                            ->children()
+                                ->booleanNode('enabled')->defaultTrue()->end()
+                                ->stringNode('platform')
+                                    ->info('Service name of the speech platform (e.g. ai.platform.elevenlabs)')
+                                    ->defaultNull()
+                                ->end()
+                                ->stringNode('tts_model')
+                                    ->info('Text-to-speech model name')
+                                    ->defaultNull()
+                                ->end()
+                                ->variableNode('tts_options')
+                                    ->info('Provider-specific TTS options')
+                                    ->defaultValue([])
+                                ->end()
+                                ->stringNode('stt_model')
+                                    ->info('Speech-to-text model name')
+                                    ->defaultNull()
+                                ->end()
+                                ->variableNode('stt_options')
+                                    ->info('Provider-specific STT options')
+                                    ->defaultValue([])
+                                ->end()
+                            ->end()
+                            ->validate()
+                                ->ifTrue(static fn (array $v): bool => ($v['enabled'] ?? false) && null === ($v['platform'] ?? null))
+                                ->thenInvalid('The "platform" must be configured when speech is enabled.')
+                            ->end()
+                            ->validate()
+                                ->ifTrue(static fn (array $v): bool => ($v['enabled'] ?? false) && null === ($v['tts_model'] ?? null) && null === ($v['stt_model'] ?? null))
+                                ->thenInvalid('At least one of "tts_model" or "stt_model" must be configured when speech is enabled.')
+                            ->end()
+                        ->end()
                     ->end()
                 ->end()
             ->end()